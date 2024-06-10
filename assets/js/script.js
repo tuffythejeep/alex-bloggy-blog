18f952faae69e1518f84621d45a0314ea4e7a3ac
tuffythejeep <>
2024-06-10 13:09:35 -0500

    adjusted html and other things

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,21 @@ themeSwitcher.addEventListener("click", function () {
     container.setAttribute("class", "dark");
   }
 });
+
+const form = document.getElementById("blog-form");
+const previewContainer = document.getElementById("blog-preview");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const blogTitle = document.getElementById("blog-title").value;
+  const blogContent = document.getElementById("blog-content").value;
+
+  const blogHTML = `
+		<h1>${blogTitle}</h1>
+		<p>By ${username}</p>
+		<p>${blogContent}</p>
+	`;
+
+  previewContainer.innerHTML = blogHTML;
+});
